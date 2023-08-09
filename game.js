4080fe332a58c1b68e64ed852ea8373d38362d59
CEO <ekimran@hotmail.com>
2023-08-09 07:01:58 -0600

    Video 3, se crean la funcion de startGame

---
+++ game.js
@@ -0,0 +1,16 @@
+ const canvas = document.querySelector('#game');
+ const game = canvas.getContext('2d');
+
+
+ window.addEventListener('load', startGame);
+ function startGame(){
+    //game.fillRect(0,0,100,1000);
+    //game.cleanRect(50,50,50,50);
+    //game.cleanRect();
+    //game.cleanRect(50,50,50,50);
+
+    game.font - '25px Vernada';
+    game.fillStyle = 'purple';
+    game.textAlign = 'left';
+    game.fillText( 'Pandamonio', 25, 25);  
+ }
